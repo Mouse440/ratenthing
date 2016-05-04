da79813d6e677519e1e18aed5484a4fff2dccddc
duynguyen0428 <duynguyen0428@gmail.com>
2016-05-04 14:39:23 +0000

    test

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var passportConfig = require('./auth/passport-config');
 var restrict = require('./auth/restrict')
 passportConfig();
 
-mongoose.connect(process.env.MONGOLAB_URI ||config.mongoUri);
+mongoose.connect(process.env.MONGOLAB_URI || config.mongoUri);
 var app = express();
 
 // view engine setup
